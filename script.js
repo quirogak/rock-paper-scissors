1efb97d187c571b6b2edc464bd9cdc8edb77c727
quirogak <quirogakill@gmail.com>
2022-07-18 14:29:32 -0500

    Successfully made getComputerChoice,GetUserChoice.
    Made them no case-sensitive and created a statement which makes the user input to choose only three posible options.

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("Hello World");
+function getComputerChoice(){
+    const options =["Rock", "Paper","Scissors"]
+
+    const random = options[Math.floor(Math.random() * options.length)];
+    return random;
+  
+}
+
+function getUserChoice(){
+
+   let userChoice = prompt("Please select an option between Paper, Rock and Scissors");
+
+   let firstLetter = userChoice.charAt(0).toUpperCase();
+
+   let restText = userChoice.slice(1).toLowerCase();
+
+   let fullText = firstLetter + restText; 
+
+   if (fullText != "Rock" && fullText != "Paper" && fullText != "Scissors" ){
+    alert("Please put a correct value")
+    return undefined
+
+ } else {
+    return fullText;
+}
+
+   }
+
+
+   
+
+
+
+
+
+
+console.log (getComputerChoice())
+
+console.log (getUserChoice())
+
