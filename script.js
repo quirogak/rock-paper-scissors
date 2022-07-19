9dcf6b7e6ecb77e64ee030e13b085fc7c92a8c29
quirogak <quirogakill@gmail.com>
2022-07-19 15:05:26 -0500

    Made variables that save different results.
    These variables store the random results of the computer and the variables that the user inputs.
    Also figured out how to define a winner with conditionals and i.
    
    There is just a bug remaining to fix, some results are not showing up in some rounds.

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ function getComputerChoice(){
     const options =["Rock", "Paper","Scissors"]
 
     const random = options[Math.floor(Math.random() * options.length)];
+    
     return random;
-  
+   
 }
 
 function getUserChoice(){
 
+   
    let userChoice = prompt("Please select an option between Paper, Rock and Scissors");
 
    let firstLetter = userChoice.charAt(0).toUpperCase();
@@ -26,20 +28,11 @@ function getUserChoice(){
 
    }
 
-        
-
-   const playerSelection = getUserChoice();
-   const computerSelection = getComputerChoice();
-   let userScore=(0)
-   let computerScore=(0)
-
 
 function playRound(playerSelection, computerSelection){
- 
-    
 
     if (playerSelection == computerSelection){
-        return 3 && "TIE!" ;
+        return "TIE!" ;
     } 
 
     else if(playerSelection == "Rock" && computerSelection == "Paper"){
@@ -68,36 +61,43 @@ function playRound(playerSelection, computerSelection){
     }
     else return computerScore++ && ("Invalid input, you lose this round.")
 
+}
     
-    
-    } 
 
-       
-
-
-    
+    function playGame(playRound) {
      
     for (let i = 0; i < 5; i++) {
         console.log(playRound(playerSelection, computerSelection));
-        console.log(userScore)
-        console.log(computerScore)
-         getUserChoice();
-         getComputerChoice();
+        console.log(userScore);
+        console.log(computerScore);
+        playerSelection = getUserChoice();
+        computerSelection = getComputerChoice();
+        
+        
          
-         if (userScore == 5){
+         if (userScore > computerScore && i == 4){
           console.log("You won the game!")
 
         }
-        if (computerScore == 5){
+        else if (computerScore > userScore && i == 4){
             console.log("You lost the game!")
   
           }
 
+        else if (userScore == computerScore && i == 4)
+        console.log("The game ends up tied!");
         
+
+        }
          }
     
-        
-        
     
+   
+    let playerSelection = getUserChoice();
+    let computerSelection = getComputerChoice();
+    let userScore=(0)
+    let computerScore=(0)
+        
+    console.log(playGame(playRound));
      
     
